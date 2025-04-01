91c08555449d00eec056d2e2a9bc656c81e96906
jyw0936 <jyw0936@naver.com>
2025-04-01 12:16:40 +0900

    Update ppurio.js

--- api/ppurio.js
+++ api/ppurio.js
@@ -5,8 +5,18 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: 'Method not allowed' });
   }
 
+  // ✅ 요청에서 필요한 데이터 추출
+  const { phonenumber, msg, template_code } = req.body;
+
   try {
-    const result = await axios.post('https://api.ppurio.com/message/v1/send/kakao', req.body, {
+    const result = await axios.post('https://api.ppurio.com/message/v1/send/kakao', {
+      userid: 'yyyk2',
+      callback: '01050777627',
+      msg,
+      phonenumber,
+      template_code,
+      pf_key: '64343e8d21e198d4c6e13e266bc77efb2ce32b8625adc567447bb13f4f813590'
+    }, {
       headers: {
         'Content-Type': 'application/json'
       }
@@ -18,3 +28,4 @@ export default async function handler(req, res) {
     return res.status(500).json({ success: false, error: error.response?.data || error.message });
   }
 }
+
